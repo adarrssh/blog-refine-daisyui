dd4e829041b5d8e9c97d96896d206ee2f858bd72
adarrssh <adarsh00502@gmail.com>
2024-03-03 17:21:50 +0530

    refactor : AvailableDates.tsx

--- src/components/dashboard/AvailableDates.tsx
+++ src/components/dashboard/AvailableDates.tsx
@@ -1,9 +1,5 @@
-export const dates = [
-    "Oct 1, 2022",
-    "Dec 1, 2022",
-    "Feb 1, 2023",
-    "Apr 1, 2023",
-    "Jun 1, 2023",
-    "Aug 1, 2023",
-    "Oct 1, 2023",
-  ];
+import { originalDashboardData } from "./DashboardData";
+
+const dates = originalDashboardData.map((data)=> data.date)
+
+export { dates }
